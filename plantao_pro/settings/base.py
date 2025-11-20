b31bd5c73826fbae591340b252df37e0ffad9792
goutemberg <gouberg@gmail.com>
2025-11-19 22:57:59 -0300

    ajustes

--- plantao_pro/settings/base.py
+++ plantao_pro/settings/base.py
@@ -57,7 +57,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "home" / "static",   
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
